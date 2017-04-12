5a0de0ec9d26ce912ad560c53627faea3e47aaf6
josedesouza <junie58@l8oaypr.com>
2017-04-12 11:37:27 +0100

    improved commenting of visualiser

--- visualiser.py
+++ visualiser.py
@@ -1,12 +1,8 @@
-
 import json
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
-
-
-
 
 class TwitterVisualiser(object):
     """Loads in a .txt file of tweets and parses into pandas dataframe for visualisation
@@ -19,7 +15,7 @@ class TwitterVisualiser(object):
         self.tweets_df = self.create_tweet_dataframe(self.data)
         
     def load_data(self, path_to_data):
-        # Load in tweet dump file
+        """ Load in tweet dump file """
         tweets_data_path = path_to_data
         tweets_data = []
         tweets_file = open(tweets_data_path, "r")
@@ -32,6 +28,7 @@ class TwitterVisualiser(object):
         return tweets_data
         
     def create_tweet_dataframe(self, tweets_data):
+        """Parse tweets into pandas data frame"""
         tweets = pd.DataFrame()
         tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
         tweets['lang'] = map(lambda tweet: tweet['lang'], tweets_data)
@@ -41,7 +38,7 @@ class TwitterVisualiser(object):
         tweets.drop('timestamp', inplace=True, axis=1)
         return tweets
     def plot_volume(self):
-        # Plot volume of tweets over time
+        """ Plot volume of tweets over time """
         tweets_df = self.tweets_df
         fig, ax = plt.subplots(1,1,figsize=(8,5))
         tweets_df[tweets_df.lang == 'en']['text'].groupby(pd.TimeGrouper(freq='1min')).count().plot(kind='line', ax=ax, label='en')
@@ -52,7 +49,7 @@ class TwitterVisualiser(object):
         plt.show()
         
     def plot_country(self):
-        # PLOT Country
+        """ Plots summary of tweet country of origin"""
         tweets_df = self.tweets_df
         fig, ax = plt.subplots(1,1,figsize=(4,2))
         tweets_df['country'].value_counts().plot(ax=ax, kind='bar', 
@@ -61,7 +58,7 @@ class TwitterVisualiser(object):
         plt.show()
         
     def plot_language(self):
-        # PLot language
+        """ Plots summary of language of tweets"""
         tweets_df = self.tweets_df
         fig, ax = plt.subplots(1,1,figsize=(6,3))
         tweets_df['lang'].value_counts().plot(ax=ax, kind='bar', 
@@ -70,9 +67,10 @@ class TwitterVisualiser(object):
                                                  
                                               
 if __name__ == '__main__':
+    # 1. Create visualiser 
     data_path = './all_tennis_tweets.txt'
     visualiser = TwitterVisualiser(data_path)
-    # DO plots
+    # 2. Make plots of loaded twitter data
     visualiser.plot_volume()
     visualiser.plot_country()
     visualiser.plot_language()
